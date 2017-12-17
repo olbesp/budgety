c2d978d09f982bf045c600def321ed4deb394bf9
Oleksii Bespalko <olbesp365@gmail.com>
2017-12-17 21:58:58 +0200

    Remove an item from data structure

--- app.js
+++ app.js
@@ -54,6 +54,16 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+      var index = ids.indexOf(id);
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
+
     calculateBudget: function() {
       // calculate total inc and exp
       calculateTotal('exp');
@@ -214,9 +224,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     if (itemID) {
       splitID = itemID.split('-');
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
 
       // 1. delete the item from the data structure
+      budgetCtrl.deleteItem(type, ID);
       // 2. delete the item from the UI
       // 3. update and show the new budget
     }

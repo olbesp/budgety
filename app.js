5bc2796137bf1ee88a262627a33fd5f457f048b2
Oleksii Bespalko <olbesp365@gmail.com>
2017-12-18 12:02:34 +0200

    Add current date into the UI

--- app.js
+++ app.js
@@ -135,7 +135,8 @@ var UIController = (function() {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
   }
 
   var formatNumber = function(num, type) {
@@ -244,6 +245,15 @@ var UIController = (function() {
       });
     },
 
+    displayMonth: function() {
+      var now = new Date();
+      var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                    'August', 'September', 'October', 'November', 'December'];
+      var month = now.getMonth();
+      var year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -320,6 +330,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   return {
     init: function() {
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,

61c950aeba4e6c03e4e9781a9b4d80b4aaaed4eb
Oleksii Bespalko <olbesp365@gmail.com>
2017-12-18 10:45:19 +0200

    Calculate percentages of every expenses

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ var budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
+  };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+    if (totalIncome > 0) {
+      this.percentage = Math.round(this.value / totalIncome * 100);
+    } else {
+      this.percentage = -1;
+    }
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
   };
 
   var Income = function(id, description, value) {
@@ -76,6 +89,19 @@ var budgetController = (function() {
       } else {
         data.percentage = -1;
       }
+    },
+
+    calculatePercentages: function() {
+      data.allItems.exp.forEach(function(current) {
+        current.calcPercentage(data.totals.inc);
+      });
+    },
+
+    getPercentages: function() {
+      var allPerc = data.allItems.exp.map(function(current) {
+        return current.getPercentage();
+      });
+      return allPerc;
     },
 
     getBudget: function() {
@@ -211,6 +237,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     UICtrl.displayBudget(budget);
   }
 
+  var updatePercentages = function() {
+    // 1. calculate percentages
+    budgetCtrl.calculatePercentages();
+    // 2. read percentages from the budget controller
+    var percentages = budgetCtrl.getPercentages();
+    // 3. update the UI with the new percentages
+    console.log(percentages);
+  };
+
   var ctrlAddItem = function() {
     // 1. Get the field input data
     var input = UICtrl.getInput();
@@ -224,6 +259,8 @@ var controller = (function(budgetCtrl, UICtrl) {
       UICtrl.clearFields();
       // 5. Calculate and update budget
       updateBudget();
+      // 6. Calculate and update percentages
+      updatePercentages();
     }
   };
 
